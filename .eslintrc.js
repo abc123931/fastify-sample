c9b770b5f02ec38301e15259d2f0a9d637068ae4
abc123931 <abc123931@gmail.com>
2021-01-19 18:06:39 +0900

    🔧 Add config files.

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: 2019, sourceType: "module" },
+  env: { es6: true, jest: true, node: true },
+  plugins: ["import", "simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  rules: {
+    "no-console": ["error", { allow: ["warn", "info", "error"] }],
+    "no-restricted-syntax": [
+      "error",
+      { selector: "TSEnumDeclaration", message: "Don't declare enums" },
+    ],
+    "prefer-arrow-callback": "error",
+    "func-style": ["error", "expression"],
+    "arrow-body-style": ["error", "always"],
+    "import/newline-after-import": "error",
+    "import/no-default-export": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { prefer: "type-imports" },
+    ],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+  },
+};
